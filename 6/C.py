49beff3edf798e4b9ffae7b366f4fc86811f1ef5
4EFI <kvv204@gmail.com>
2023-04-27 10:27:16 +0300

    Special for you - Artem

--- 6/C.py
+++ 6/C.py
@@ -1,7 +1,6 @@
 
 import RPi.GPIO as GPIO
 import time
-import pygame
 
 def dec2bin( value ):
     return [int(elem) for elem in bin(value)[2:].zfill(8)]
@@ -34,15 +33,30 @@ try:
     file  = open( "data.txt", "w" )
 
     dt = 0.1
+    k  = 0
 
-    v = []
+    v_arr = [0]
+    t_arr = [0]
 
     while( True ):
         dt_curr = time.time() - start
+        out     = adc( dac, comp )
+        v       = out / 256.0 * 3.3
+
         if( dt_curr >= dt ):
             start = time.time()
-            v.append( adc( dac, comp ) / 256.0 * 3.3 )
-            print( "Voltage is", "{:.3f}".format(adc( dac, comp ) / 256.0 * 3.3, 3) )
+            k += dt
+            v_arr.append( v )
+            t_arr.append( k )
+
+            print( "Voltage is", "{:.3f}".format( v, 3 ) )
+
+        if( v >= 3 ):
+            break
+    
+    for i in range( len( v_arr ) ):
+        file.write( "{:.2f} ".format( t_arr[i], 3 ) )
+        file.write( "{:.5f}\n".format( v_arr[i], 3 ) )
 
 except KeyboardInterrupt:
     print( "\nZachem menia prerval?? AA?" ) 
